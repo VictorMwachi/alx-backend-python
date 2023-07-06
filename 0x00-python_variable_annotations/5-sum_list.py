801258eafa63da19454ddd0a3c0724c7dbb059ea
Victor Mwachi <mudakizevictor@gmail.com>
2023-07-06 12:44:50 +0000

    add float list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,6 +2,9 @@
 """5. Complex types - list of floats """
 
 
-def sum_list(input_list: list[float]) -> float:
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
     """sum of floats"""
     return sum(input_list)
