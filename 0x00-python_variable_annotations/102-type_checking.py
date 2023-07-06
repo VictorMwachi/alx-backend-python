b1e168ec769e564e2db7989f6382e47911308071
victormwachi <mudakizevictor@gmail.com>
2023-07-07 00:24:36 +0300

    fixed

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -12,6 +12,8 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
             for i in range(factor)
             ]
     return zoomed_in
+
+
 array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
