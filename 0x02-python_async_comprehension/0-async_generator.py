fdfd04662f556d8a9fc44db23070deb70f89a916
Victor Mudaki Mwachi <20131567+VictorMwachi@users.noreply.github.com>
2023-07-14 08:47:35 +0300

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator() -> Generator[float, None, None]:
     """ yield a random number between 0 and 10"""
     for i in range(10):
         await asyncio.sleep(1)
-        yield random.randrange(0, 10)
+        yield random.uniform(0, 10)
