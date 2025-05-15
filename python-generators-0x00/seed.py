4a04634ea92775dddf572f73ef8c500d3aff54f3
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-15 13:02:21 +0300

    connection to  db

--- python-generators-0x00/seed.py
+++ python-generators-0x00/seed.py
@@ -1,5 +1,19 @@
+import mysql.connector
+from mysql.connector import Error
+
 def connect_db():
-    pass
+    try:
+        connection = mysql.connector.connect(
+        host = "localhost",
+        user = "admin",
+        database = "ALX_prodev",
+        password = "adminpassword"
+        )        
+        if connection.is_connected():
+            print("Connected to MySQL database")
+        return connection
+    except Error as e:
+        print(f"e")
 def connect_to_prodev():
     pass
 def create_database():
@@ -8,3 +22,5 @@ def create_table(connection):
     pass
 def insert_data(connection, data):
     pass
+if __name__ == '__main__':
+    connect_db()
