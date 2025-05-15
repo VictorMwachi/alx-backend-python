59a1318531a84dea7e4667605f43b95782de4834
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-15 12:03:26 +0300

    set up mysql using docker

---
+++ python-generators-0x00/seed.py
@@ -0,0 +1,10 @@
+def connect_db():
+    pass
+def connect_to_prodev():
+    pass
+def create_database():
+    pass
+def create_table(connection):
+    pass
+def insert_data(connection, data):
+    pass
