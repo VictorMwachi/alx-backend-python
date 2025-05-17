682c5a65d01552169d5d68f6a8e2b5a6afd4b76d
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-18 02:40:20 +0300

    tested to return a dictionary

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -8,5 +8,15 @@ def stream_users():
         password = "adminpassword"
     )
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM user_data LIMIT 10")
-    yield cursor.fetchone()
+    cursor.execute("SELECT * FROM user_data")
+    for user in cursor.fetchall():
+        yield {
+            'user_id':user[0],
+            'name':user[1],
+            'email':user[2],
+            'age':user[3]
+            }
+if __name__ == '__main__':
+    from itertools import islice
+    for user in islice(stream_users(), 6):
+        print(user)
