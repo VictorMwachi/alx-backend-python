a0a80380bbaa8f3e6cbf35888aacc3bb2b994d24
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-18 03:48:07 +0300

    update

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -9,10 +9,11 @@ def stream_users_in_batches(batch_size):
         database = 'ALX_prodev',
         password = 'adminpassword',
         )
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM user_data")
-    yield cursor.fetchmany(size=batch_size)
+    cursor = connection.cursor(dictionary=True)
+    offset = 0
+    #cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s"(batch_size, offset))
+    #yield cursor.fetchmany(size=batch_size)
 
 
-#def batch_processing(batch):
-print(stream_users_in_batches(batch_size=50))
+def batch_processing(batch):
+    print(stream_users_in_batches(batch_size=50))
