9d213864440642b3578d703ba01737d4ef333648
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-24 15:00:55 +0300

    finalised task 0

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,13 +2,17 @@ import sqlite3
 import functools
 from datetime import datetime
 
-#### decorator to lof SQL queries
 
-""" YOUR CODE GOES HERE"""
 def log_queries(func):
+    @functools.wraps(func)
     def wrapper(*args,**kwargs):
+        query = kwargs.get('query') or args[0]
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        if query:
+            print(f"[{timestamp}] Executing query: {query}")
+        else:
+            print(f"[{timestamp}] No query to execute")
+    
         connect = func(*args,**kwargs)
-        print(datetime.now(),result)
-        
         return connect
     return wrapper
