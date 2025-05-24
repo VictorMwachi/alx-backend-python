c5087f6bf3c55f2ec06bfac3a0d3299e0f5d6b32
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-24 15:20:23 +0300

    automatic db connection'

---
+++ python-decorators-0x01/1-with_db_connection.py
@@ -0,0 +1,11 @@
+import sqlite3
+import functools
+
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args,**kwargs):
+        conn = sqlite3.connect('users.db')
+        result = func(*args,**kwargs)
+        conn.close()
+        return result
+    return wrapper
