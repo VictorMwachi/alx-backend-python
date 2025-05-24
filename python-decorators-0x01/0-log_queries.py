15ba88a47d52081b044153233559ef437ed8a2cc
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-24 13:38:59 +0300

    decorator

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,10 +1,12 @@
 import sqlite3
 import functools
+import datetime
 
 #### decorator to lof SQL queries
 
 """ YOUR CODE GOES HERE"""
-def log_queries():
+def log_queries(func):
     def wrapper(*args,**kwargs):
-        pass
-    pass
+        result = func(*args,**kwargs)
+        return result
+    return wrapper
