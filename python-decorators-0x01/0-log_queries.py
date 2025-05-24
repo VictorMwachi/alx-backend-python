a0c9a2494e7a62fd4990a28b23d62997c97e01b1
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-24 14:12:31 +0300

    added connect variable

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,13 +1,15 @@
 import sqlite3
 import functools
-import datetime
+from datetime import datetime
 
 #### decorator to lof SQL queries
 
 """ YOUR CODE GOES HERE"""
 def log_queries(func):
     def wrapper(*args,**kwargs):
+        print(datetime.now())
         result = func(*args,**kwargs)
-        print(result)
+        print(datetime.now(),result)
+        print(datetime.now())
         return result
     return wrapper
