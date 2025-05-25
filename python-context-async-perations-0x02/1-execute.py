9dc8cfa5a099d71fb4752cb9e27bd97dcca72076
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-25 23:58:37 +0300

    update'

--- python-context-async-perations-0x02/1-execute.py
+++ python-context-async-perations-0x02/1-execute.py
@@ -1,15 +1,22 @@
 import sqlite3
+
+
 class ExecuteQuery:
-    def __init__(self, query, param):
+    def __init__(self, db_path, query, param):
+        self.db_path = db_path
         self.query = query
         self.param = param
+        self.conn = None
+        self.cursor = None
 
     def __enter__(self):
-        self.conn = sqlite3.connect(self.db)
-        self.cursor = db_conn.cursor()
-        self.cursor.execute(self.query,(self.param,))
+        self.conn = sqlite3.connect(self.db_path)
+        self.cursor = self.conn.cursor()
+        self.cursor.execute(self.query, (self.param,))
         return self.cursor.fetchall()
 
-    def __exit__(self,type,value,traceback):
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.cursor:
+            self.cursor.close()
         if self.conn:
             self.conn.close()
