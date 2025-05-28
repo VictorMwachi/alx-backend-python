c38a20a52380309d55bba3a7726db6f2282888f3
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-28 13:04:45 +0300

    raises key error

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,6 +5,9 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """
+    Test Cases for access_nested_map function
+    """
     @parameterized.expand([
         ("test1", {"a": 1}, ("a",), 1),
         ("test2", {"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -12,3 +15,11 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, name, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+    
+    @parameterized.expand([
+        ("Empty Dictionary", {}, ("a",)),
+        ("Missing Key", {"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, name, nested_map, path):
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
