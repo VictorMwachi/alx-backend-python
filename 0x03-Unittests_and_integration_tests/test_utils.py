9dd51ddd6ebdac440f677c368ec8a721a1da1632
Victor Mwachi <mudakizevictor@gmail.com>
2025-05-28 12:31:34 +0300

    adhere to pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,11 +3,12 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
-        ("test",{"a": 1}, ("a",),1),
-        ("test2",{"a": {"b": 2}}, ("a",),{"b":2}),
-        ("test3",{"a": {"b": 2}}, ("a", "b"),2)
+        ("test1", {"a": 1}, ("a",), 1),
+        ("test2", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("test3", {"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self,name,nested_map,path,expected):
-        self.assertEqual(access_nested_map(nested_map, path),expected)
+    def test_access_nested_map(self, name, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
