5aff2c6ddc19a0161979c1ff1c8a1baac878292a
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-02 13:39:20 +0300

    update restframework authentetict

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ]
