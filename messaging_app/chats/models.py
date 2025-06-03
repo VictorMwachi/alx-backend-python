29fb9b551d9a1b6c4e9ad007369e08b16161d5c8
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-03 12:21:00 +0300

    update user

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class user(AbstractUser):
     user_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    first_name = models.CharField(max_length=25,null=False)
+    last_name = models.CharField(max_length=25,null=False)
+    password = models.CharField(min_length=8,null=False)
     phone_number = models.CharField(max_length=15)
 
     def __str__(self):
