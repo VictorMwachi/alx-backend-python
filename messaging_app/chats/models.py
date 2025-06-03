bfd300a5ac823f0dcbbb487ea754ec55a32b3c26
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-03 12:15:30 +0300

    user model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,8 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class user(models.Model):
-    pass
+class user(AbstractUser):
+    user_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    phone_number = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.email
+    
 class message(models.Model):
     pass
 class conversation(models.Model):
