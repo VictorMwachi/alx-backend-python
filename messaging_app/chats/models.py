4a88f0dd44962e894d9a042bf9d9d649ef2709fa
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-04 13:20:04 +0300

    conversation and mesage model

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -13,7 +13,20 @@ class user(AbstractUser):
     def __str__(self):
         return self.email
     
-class message(models.Model):
-    pass
-class conversation(models.Model):
-    pass
+class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    users = models.ManyToManyField(User,related_name='conversation')
+
+    def __str__(self):
+        return self.conversation_id
+    
+class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    user = models.ForeignKey(user,on_delete=models.CASCADE)
+    conversation = models.ForeignKey(Conversation,on_delete=models.CASCADE)
+    message_body = models.CharField(null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message_id
