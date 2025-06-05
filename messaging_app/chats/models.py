c7339020a7f3c955223f9478a4f502a3177ddfae
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-05 11:47:33 +0300

    complete serializers

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
 class User(AbstractUser):
     user_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    username = None
+    email = models.EmailField(unique=True,null=False)
     first_name = models.CharField(max_length=25,null=False)
     last_name = models.CharField(max_length=25,null=False)
-    password = models.CharField(min_length=8,null=False)
-    phone_number = models.CharField(max_length=15)
+    password = models.CharField(max_length=128,validators=[MinLengthValidator(8)])
+    phone_number = models.CharField(max_length=15,null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
 
     def __str__(self):
         return self.email
@@ -18,7 +24,7 @@ class Conversation(models.Model):
     users = models.ManyToManyField(User,related_name='participants')
 
     def __str__(self):
-        return self.conversation_id
+        return str(self.conversation_id)
     
 class Message(models.Model):
     message_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
@@ -29,4 +35,4 @@ class Message(models.Model):
     sent_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.message_id
+        return f"Message {self.message_id} sent at: {self.sent_at} by: {self.user}"
