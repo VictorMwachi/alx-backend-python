49ba60752eb9ec5c8a48ce169c9ce72250231ddb
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-08 23:28:50 +0300

    basepermission

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,1 +1,10 @@
-from rest_framework import permissions, BasePermission
+from rest_framework.permissions import BasePermission
+from .models import Conversation
+
+class IsParticipantOfConversation(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if hasattr(obj, 'participants'):
+            return request.user in obj.participants.all()
+        if hasattr(obj, 'conversation'):
+            return request.user in obj.conversation.participants.all()
+        return False
