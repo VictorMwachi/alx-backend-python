0c39cb1bc4d30e23530c817047a085065624dd46
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-08 15:55:18 +0300

    urls

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('api/',include('chats.urls')),
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
 ]
