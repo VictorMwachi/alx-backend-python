c9fb185651e7890b4a8cd96f40d404e1dfcb3e0b
Victor Mwachi <mudakizevictor@gmail.com>
2025-06-15 12:35:05 +0300

    participants

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -43,7 +43,7 @@ class User(AbstractUser):
     
 class Conversation(models.Model):
     conversation_id = models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
-    users = models.ManyToManyField(User,related_name='conversations')
+    participants = models.ManyToManyField(User,related_name='conversations')
 
     def __str__(self):
         return str(self.conversation_id)
